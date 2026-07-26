361295f0d0c7e7323b6742b59a8d548c68586b8c
michail <michail.pa.2003@gmail.com>
2026-07-26 21:09:27 +0200

    eme

--- updateBarberPhone.js
+++ updateBarberPhone.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Barber = require("./models/Barber"); // adjust path
 
-mongoose.connect("mongodb://michail:FIFA19ea@ac-abruomg-shard-00-00.e4mecnf.mongodb.net:27017,ac-abruomg-shard-00-01.e4mecnf.mongodb.net:27017,ac-abruomg-shard-00-02.e4mecnf.mongodb.net:27017/?ssl=true&replicaSet=atlas-f8guf2-shard-0&authSource=admin&appName=Michail-Production")
+mongoose.connect("")
     .then(async () => {
         const result = await Barber.updateMany(
             { ph_number: { $exists: true } },
